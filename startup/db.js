97a2d865fe50f768d5ed71ee2ab54871ee03cae9
Jon Fleet <jmfleet1@gmail.com>
2020-10-19 18:38:07 -0400

    Added support for reading a connection string from an environment variable

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose")
+const config = require('config')
 
 
 module.exports = function () {
-    mongoose.connect('mongodb://localhost/my-wallet' , {useNewUrlParser: true})
+    mongoose.connect(config.get('db') , {useNewUrlParser: true})
 .then( () => console.log("Connected to Database"))
 .catch( (er) => console.log("Connection Error: " + er))
 } 

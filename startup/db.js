b5ee08629b09c5e47b8f15072e1be28f0e770b1b
Jon Fleet <jmfleet1@gmail.com>
2020-10-20 21:19:14 -0400

    Switch from Local to Cloud Mlab Database

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,14 @@ const config = require('config')
 
 
 module.exports = function () {
-    mongoose.connect(config.get('db') , {useNewUrlParser: true})
+    mongoose.connect(config.get('mlab') , {useNewUrlParser: true})
 .then( () => console.log("Connected to Database"))
 .catch( (er) => console.log("Connection Error: " + er))
 } 
+
+
+// module.exports = function () {
+//     mongoose.connect(config.get('db') , {useNewUrlParser: true})
+// .then( () => console.log("Connected to Database"))
+// .catch( (er) => console.log("Connection Error: " + er))
+// } 

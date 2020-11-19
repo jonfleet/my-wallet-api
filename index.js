f1dec9c354d246c0d2fc2e435a43176ce9ac5fdb
Jon Fleet <jmfleet1@gmail.com>
2020-11-19 12:52:31 -0500

    CORS Troubleshooting

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ require("./startup/db")();
 
 // CORS
 app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.header('Access-Control-Allow-Origin', '*');
         // res.header('Access-Control-Allow-Origin', 'https://dashboard.heroku.com/apps/mysterious-bayou-32982')
         // res.header('Access-Control-Allow-Origin', '*');
         // req.header('Access-Control-Allow-Origin', "*");

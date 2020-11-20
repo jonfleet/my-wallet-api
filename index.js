2a9617a11cbfaf8cda30584379d6bc968b2fb1db
Jon Fleet <jmfleet1@gmail.com>
2020-11-19 22:18:28 -0500

    CORS Troubleshooting

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ require("./startup/db")();
 app.use((req, res, next) => {
         // res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Origin', 'https://quiet-atoll-26675.herokuapp.com/')
-        // res.header('Access-Control-Allow-Origin', '*');
+        res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+        // Set custom headers for CORS
+        res.header("Access-Control-Allow-Headers", "Content-Type,Accept,x-auth-token")
         // req.header('Access-Control-Allow-Origin', "*");
         // console.log(response)
         next();

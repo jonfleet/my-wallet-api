e51b8d83d81c10c47f95ac76a224da39cd84d149
Jon Fleet <jmfleet1@gmail.com>
2020-11-19 20:20:08 -0500

    CORS Troubleshooting

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
         // res.header('Access-Control-Allow-Origin', 'https://quiet-atoll-26675.herokuapp.com/')
         // res.header('Access-Control-Allow-Origin', '*');
         // req.header('Access-Control-Allow-Origin', "*");
-        console.log(response)
+        // console.log(response)
         next();
 });
 

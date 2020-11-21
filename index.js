68d719464a813fafb2ca801ae2fd29b1fd5316ba
Jon Fleet <jmfleet1@gmail.com>
2020-11-21 16:33:54 -0500

    Changed CORS policy URL to whispering-shore

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ require("./startup/db")();
 app.options("*", cors())
 
 app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', 'https://quiet-atoll-26675.herokuapp.com')
+        res.header('Access-Control-Allow-Origin', 'https://whispering-shore-41464.herokuapp.com')
         next();
 });
 
